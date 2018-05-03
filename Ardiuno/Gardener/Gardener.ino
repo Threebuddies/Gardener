27d4412308336f359882e22b606ba645a131f1d8
BuildTools <unconfigured@null.spigotmc.org>
2018-05-03 14:48:49 -0400

    Web Server initial
    Basics from the web client example

--- Ardiuno/Gardener/Gardener.ino
+++ Ardiuno/Gardener/Gardener.ino
@@ -1,13 +1,4 @@
 /*
-  Reads analog sensors on A0 - A5 and prints to serial monitor
-  A 16x2 LCD display.  The LiquidCrystal
-  library works with all LCD displays that are compatible with the
-  Hitachi HD44780 driver. There are many of them out there, and you
-  can usually tell them by the 16-pin interface.
-  Prints "Hello World!" to the LCD and uses the
-  display() and noDisplay() functions to turn on and off
-  the display.
-
   The circuit:
   * LCD RS pin to digital pin 12
   * LCD Enable pin to digital pin 11
@@ -19,23 +10,21 @@
   * 10K resistor:
   * ends to +5V and ground
   * wiper to LCD VO pin (pin 3)
-  http://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
   A0 - A5 connected to the sensors
+  sensor 1 = external temp
+  sensor 2 = external humidity
+  sensor 3 = internal temp
+  sensor 4 = internal humidity
 */
-
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <Ethernet.h>
-
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-
 IPAddress ip(192, 168, 1, 177);
 EthernetServer server(80);
-
-// Initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to.
+// set up the lcd
 const int registerSelect = 12;
 const int enablePin = 11;
 const int lcdData4 = 5;
@@ -46,11 +35,9 @@ LiquidCrystal lcd(registerSelect, enablePin, lcdData4, lcdData5, lcdData6, lcdDa
 
 void setup() {
   Serial.begin(9600);
-  // lcd
   lcd.begin(16, 2);
   lcd.print("hello, world!");
-
-  // start the Ethernet connection and server
+  // start up server
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("server is at ");
@@ -58,14 +45,14 @@ void setup() {
 }
 
 void loop() {
-  //Inialize all the analog pins
+  //Inialize all the analog sensor pins
+
   int sensorValue0 = analogRead(A0);
   int sensorValue1 = analogRead(A1);
   int sensorValue2 = analogRead(A2);
   int sensorValue3 = analogRead(A3);
   int sensorValue4 = analogRead(A4);
   int sensorValue5 = analogRead(A5);
-  
   // Prints all the sensor values to serial
   Serial.println("Sensor 0 value = " + sensorValue0);
   Serial.println("Sensor 1 value = " + sensorValue1);
@@ -77,46 +64,45 @@ void loop() {
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
-    // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Connection: close");
+          client.println("Refresh: 5");
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
+
+          int sensorValue0 = analogRead(A0);
+          int sensorValue1 = analogRead(A1);
+          int sensorValue2 = analogRead(A2);
+          int sensorValue3 = analogRead(A3);
+          int sensorValue4 = analogRead(A4);
+          int sensorValue5 = analogRead(A5);
+          // Prints all the sensor values to server
+          client.println("Sensor 0 value = " + sensorValue0);
+          client.println("Sensor 1 value = " + sensorValue1);
+          client.println("Sensor 2 value = " + sensorValue2);
+          client.println("Sensor 3 value = " + sensorValue3);
+          client.println("Sensor 4 value = " + sensorValue4);
+          client.println("Sensor 5 value = " + sensorValue5);
           client.println("</html>");
           break;
         }
+        //checks to see if the current line is blank
         if (c == '\n') {
-          // you're starting a new line
           currentLineIsBlank = true;
         } else if (c != '\r') {
-          // you've gotten a character on the current line
           currentLineIsBlank = false;
         }
       }
     }
-    // give the web browser time to receive the data and to limit serial spam
+    // limit output spam
     delay(1);
-}
+  }
 }
